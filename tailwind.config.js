2b113f81c2a221553fd93e1c01ce530abe35eaea
JoshuaCleverley <JoshuaCleverley@protonmail.com>
2023-05-13 00:28:35 +0100

    Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "hsl(259, 100%, 65%)",
+        secondary: "hsl(0, 100%, 67%)",
+
+        neutral: {
+          100: "hsl(0, 0%, 100%)",
+          200: "hsl(0, 0%, 94%)",
+          300: "hsl(0, 0%, 86%)",
+          400: "hsl(0, 1%, 44%)",
+          500: "hsl(0, 0%, 8%)",
+        },
       },
     },
   },
   plugins: [],
-}
+};
