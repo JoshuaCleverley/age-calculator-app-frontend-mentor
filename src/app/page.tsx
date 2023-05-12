390faf80d13313a66d627b24372ee14330b30d3f
JoshuaCleverley <JoshuaCleverley@protonmail.com>
2023-05-13 00:35:09 +0100

    Starter HTML

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
 export default function Home() {
   return (
     <main className="h-screen flex flex-col justify-center items-center bg-neutral-200">
-      <h1 className="font-extrabold text-5xl text-primary">Hello, world!</h1>
+      <div>
+        <form>
+          <label htmlFor="day">DAY</label>
+          <input type="number" placeholder="DD" id="day" />
+
+          <label htmlFor="month">MONTH</label>
+          <input type="number" placeholder="MM" id="month" />
+
+          <label htmlFor="year">YEAR</label>
+          <input type="number" placeholder="YYYY" id="year" />
+
+          <button type="submit">
+            <img src="/icon-arrow.svg" />
+          </button>
+        </form>
+        <div>
+          <p>
+            <span>--</span>years
+          </p>
+          <p>
+            <span>--</span>months
+          </p>
+          <p>
+            <span>--</span>days
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
